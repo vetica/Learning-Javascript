ca9a1b834956781a122d684551ca5b795b7c7ff3
vetica <sholladay@gannett.com>
2022-11-07 10:21:45 -0500

    updates types.html and types.js

---
+++ playground/types.js
@@ -0,0 +1,11 @@
+const first = "Spencer";
+const middle = "Dale";
+const last = "Holladay";
+
+// const hello = "hello my name is " + first + ". Nice to meet you";
+
+// let hello2 = "hello my name is ";
+// hello2 = hello2 + first + " " + last;
+// hello2 = hello2 + ". Nice to meet you";
+
+const hello = `hello my name is ${first} ${last}. Nice to meet you`;
