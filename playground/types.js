a9de47dde09cefc6351640a64e6892e79068922d
vetica <sholladay@gannett.com>
2022-11-07 11:27:35 -0500

    updates types.js with commented out code

--- playground/types.js
+++ playground/types.js
@@ -1,11 +1,40 @@
-const first = "Spencer";
-const middle = "Dale";
-const last = "Holladay";
+// const first = "Spencer";
+// const middle = "Dale";
+// const last = "Holladay";
 
-// const hello = "hello my name is " + first + ". Nice to meet you";
+// // const hello = "hello my name is " + first + ". Nice to meet you";
 
-// let hello2 = "hello my name is ";
-// hello2 = hello2 + first + " " + last;
-// hello2 = hello2 + ". Nice to meet you";
+// // let hello2 = "hello my name is ";
+// // hello2 = hello2 + first + " " + last;
+// // hello2 = hello2 + ". Nice to meet you";
 
-const hello = `hello my name is ${first} ${last}. Nice to meet you`;
+// const hello = `hello my name is ${first} ${last}. Nice to meet you`;
+
+// const age = 100;
+// const name = "Spencer";
+
+// // const a = 10;
+// // const b = 20;
+
+// const smarties = 20;
+// const kids = 3;
+// const eachKidGets = Math.floor(smarties / kids);
+// const dadGets = smarties % kids;
+// console.log(`Each kid gets ${eachKidGets}`);
+// console.log(`Dad gets ${dadGets}`);
+
+// const person = {
+//   first: "Spencer",
+//   last: "Holladay",
+//   age: 100,
+// };
+
+// let dog;
+// console.log(dog);
+
+// let isDrawing = false;
+// let age = 18;
+// const ofAge = age > 19;
+// console.log(ofAge);
+
+// age = 100;
