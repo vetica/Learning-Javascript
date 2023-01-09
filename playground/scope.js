b1b49c5d7091b53f00e429422b17701e6c698c6b
vetica <sholladay@gannett.com>
2023-01-09 11:30:06 -0500

    Adds scope exercises

---
+++ playground/scope.js
@@ -0,0 +1,21 @@
+// const age = 100;
+
+// function go() {
+//   const age = 200;
+//   const hair = 'blonde';
+//   console.log(age);
+//   console.log(hair);
+// }
+
+// go();
+
+function isCool(name) {
+  let cool;
+  if (name === 'spencer') {
+    cool = true;
+  }
+  console.log(cool);
+  return cool;
+}
+
+isCool('spencer');
