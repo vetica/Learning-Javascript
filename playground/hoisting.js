f6a8ad2f5cd2a13aec588afc3769e518306455e5
vetica <sholladay@gannett.com>
2023-01-11 11:10:04 -0500

    adds hoisting exercises

---
+++ playground/hoisting.js
@@ -0,0 +1,16 @@
+let age;
+console.log(age);
+age = 10;
+
+// what does this file do
+sayHi();
+
+// how does this file do it?
+function sayHi() {
+  console.log('hey');
+  console.log(add(10, 2));
+}
+
+function add(a, b) {
+  return a + b;
+}
