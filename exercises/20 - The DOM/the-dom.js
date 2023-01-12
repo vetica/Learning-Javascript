fafa2e70bf2ca9c82439b48bca6e4c5434db7f11
vetica <sholladay@gannett.com>
2023-01-12 18:54:12 -0500

    adds starting DOM files

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,14 @@
+const p = document.querySelector('p');
+const divs = document.querySelectorAll('div');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2Image = item2.querySelector('img');
+const heading = document.querySelector('h2');
+
+console.log(heading.textContent);
+console.log(heading.innerText);
+
+// set the textContent property on that element
+// heading.textContent = 'Spencer is Cool';
+// console.log(heading.textContent);
+// console.log(heading.innerText);
