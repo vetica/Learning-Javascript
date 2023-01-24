1581765803486ddadee1a21289962f125d29a12b
vetica <sholladay@gannett.com>
2023-01-24 10:19:14 -0500

    adds DOM traversing exercises

--- exercises/20 - The DOM/traversing.js
+++ exercises/20 - The DOM/traversing.js
@@ -1,2 +1,14 @@
 const spencer = document.querySelector('.spencer');
-console.log(spencer.childNodes);
+
+// console.log(spencer.children);
+// console.log(spencer.firstElementChild);
+// console.log(spencer.lastElementChild);
+// console.log(spencer.previousElementSibling);
+// console.log(spencer.nextElementSibling);
+// console.log(spencer.parentElement);
+
+const p = document.createElement('p');
+p.textContent = 'I will be removed';
+document.body.appendChild(p);
+
+p.remove();
