8777c1592d55500d11c48351dfd9070ba12ee5ea
vetica <sholladay@gannett.com>
2023-02-14 11:07:22 -0500

    adds some DOM exercises

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -41,24 +41,49 @@ myDiv.appendChild(image);
 
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
+const myHTML = `<div><p>Paragraph 1</p><p>Paragraph 2</p></div>`;
+myDiv.insertAdjacentHTML('afterbegin', myHTML);
 
 // add a class to the second paragraph called warning
 // remove the first paragraph
+const myPara = document.getElementsByTagName('p');
+const secondPara = myPara.item(1);
+secondPara.classList.add('warning');
+
+const firstPara = myPara.item(0);
+firstPara.remove();
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
-
 // have that function return html that looks like this:
 // <div class="playerCard">
 //   <h2>NAME — AGE</h2>
 //   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
 // </div>
+function generatePlayerCard(name, age, height) {
+  let dogAge = age * 7;
+  return (card = `
+    <div class="playerCard">
+        <h2>${name} — ${age}</h2>
+        <p>They are ${height} tall and ${age} years old. In Dog years this person would be ${dogAge}. That would be a tall dog!</p>
+    </div>`);
+}
 
 // make a new div with a class of cards
+const cardDiv = document.createElement('div');
+cardDiv.classList.add('cards');
 
 // make 4 player cards using generatePlayerCard
+const firstCard = generatePlayerCard('Spencer', 41, `5'11`);
+const secondCard = generatePlayerCard('Em', 28, `5'9`);
+const thirdCard = generatePlayerCard('Curry', 32, `6'1`);
+const fourthCard = generatePlayerCard('Adele', 37, `5'6`);
 
 // append those cards to the div
+
+document.getElementsByClassName('cards').innerText = firstCard;
+
 // put the div into the DOM just before the wrapper element
+document.body.insertAdjacentElement('afterbegin', cardDiv);
 // Bonus, put a delete Button on each card so when you click it, the whole card is removed
 
 // select all the buttons!
