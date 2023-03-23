51db3e0cb7d5cd66a40c823eb380c58faaef0282
vetica <sholladay@gannett.com>
2023-03-23 10:20:38 -0400

    adds event propagation exercises

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -14,13 +14,35 @@ butts.removeEventListener('click', handleClick);
 
 const buyButtons = document.querySelectorAll('button.buy');
 
-function buyItem() {
-  console.log('Buying item!');
+function handleBuyButtonClick(event) {
+  console.log('You clicked a button');
+  const button = event.target;
+  // console.log(button.textContent);
+  // console.log(parseFloat(event.target.dataset.price));
+  console.log(event.currentTarget);
+  console.log(event.target);
+  console.log(event.target === event.currentTarget);
+  // Stop this from bubbling up
+  // event.stopPropagation();
 }
 
-function loopBuy(buy) {
-  console.log('Binding the buy button');
-  buy.addEventListener('click', buyItem);
-}
+buyButtons.forEach(function (buyButton) {
+  buyButton.addEventListener('click', handleBuyButtonClick);
+});
 
-buyButtons.forEach(loopBuy);
+window.addEventListener(
+  'click',
+  function (event) {
+    console.log('YOU CLICKED THE WINDOW');
+    console.log(event.target);
+    event.stopPropagation();
+  },
+  { capture: true }
+);
+
+const photoEl = document.querySelector('.photo');
+
+photoEl.addEventListener('mouseenter', e => {
+  console.log(e.currentTarget);
+  console.log(this);
+});
